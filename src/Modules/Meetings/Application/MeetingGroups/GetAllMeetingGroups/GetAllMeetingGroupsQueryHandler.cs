c3b8aff732cf53ced4fee3551d753dcd67cb45b5
almaraubel <almar@aubel.nl>
2023-12-22 10:44:01 +0100

    fix it

--- src/Modules/Meetings/Application/MeetingGroups/GetAllMeetingGroups/GetAllMeetingGroupsQueryHandler.cs
+++ src/Modules/Meetings/Application/MeetingGroups/GetAllMeetingGroups/GetAllMeetingGroupsQueryHandler.cs
@@ -21,7 +21,7 @@ namespace CompanyName.MyMeetings.Modules.Meetings.Application.MeetingGroups.GetA
                                SELECT 
                                     [MeetingGroup].[Id] as [{nameof(MeetingGroupDto.Id)}] , 
                                     [MeetingGroup].[Name] as [{nameof(MeetingGroupDto.Name)}], 
-                                    [MeetingGroup].[Description] as [{nameof(MeetingGroupDto.Description)}] 
+                                    [MeetingGroup].[Description] as [{nameof(MeetingGroupDto.Description)}], 
                                     [MeetingGroup].[LocationCountryCode] as [{nameof(MeetingGroupDto.LocationCountryCode)}],
                                     [MeetingGroup].[LocationCity] as [{nameof(MeetingGroupDto.LocationCity)}],
                                FROM [meetings].[v_MeetingGroups] AS [MeetingGroup]
